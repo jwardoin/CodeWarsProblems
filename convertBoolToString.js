3cb0df557e960ec92cffb02f9c4f93ba1009fc80
jwardoin <jordanwardoin@gmail.com>
2022-02-21 14:01:52 -0600

    day 6

---
+++ convertBoolToString.js
@@ -0,0 +1,12 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// my original solution
+
+const booleanToString = b => b ? 'true' : 'false'
+
+
+// what I learned from others' solutions
+
+// const booleanToString = b => String(b) OR b + "" OR `${b}` OR b.toString() 
