40773cebaad58715244a55a94cb470a0615e4455
jwardoin <jordanwardoin@gmail.com>
2022-02-20 20:00:00 -0600

    day 5

---
+++ convertBooltoYesOrNo.js
@@ -0,0 +1,13 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// my original solution
+
+function boolToWord( bool ){
+    if (bool) return 'Yes'
+    else return 'No'
+}
+
+
+// what I learned from others' solutions
+
+// const boolToWord = bool => bool ? 'Yes' : 'No'
