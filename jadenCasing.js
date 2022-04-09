512a7b2a2d4e49d90ca5d2a1aeaf086008701180
jwardoin <jordanwardoin@gmail.com>
2022-04-08 22:55:17 -0500

    day 36

--- jadenCasing.js
+++ jadenCasing.js
@@ -12,5 +12,3 @@
 String.prototype.toJadenCase = function () {
     return this.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
 }
-
-// what I learned from others' solutions
