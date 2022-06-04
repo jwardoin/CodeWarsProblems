956a9496d24324fe14e34f878eb46ffc05841ee4
jwardoin <jordanwardoin@gmail.com>
2022-06-04 14:53:26 -0500

    day 72

---
+++ twoSum.js
@@ -0,0 +1,33 @@
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+// Based on: http://oj.leetcode.com/problems/two-sum/
+
+// twoSum [1, 2, 3] 4 === (0, 2)
+
+// my original solution
+
+function twoSum(numbers, target) {
+    const answer = []
+    for(let i = 0; i < numbers.length - 1; i++) {
+      for(let j = i + 1 ; j < numbers.length; j++) {
+        if(numbers[i] + numbers[j] === target) {
+          answer.push(numbers.indexOf(numbers[i]), numbers.lastIndexOf(numbers[j]))
+        }
+      }
+    }
+    return answer
+}
+
+// what I learned from others' solutions
+
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length-1; i++) {
+        for (let j = i+1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i, j] // i and j are already the indexes of the two sum answer, so there is no need to use indexOf and lastIndexOf - and instead of passing them to an array, they can just be returned immediately as an array literal
+        }
+    }
+}
