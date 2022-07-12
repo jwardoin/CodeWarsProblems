0d1f8b16c406e06c5a51c653da8268f63e23a05f
jwardoin <jordanwardoin@gmail.com>
2022-07-12 12:38:34 -0500

    day 110

---
+++ schoolPaperwork.ts
@@ -0,0 +1,20 @@
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+// Waiting for translations and Feedback! Thanks!
+
+// my original solution
+
+export function paperwork(n: number, m :number): number{
+    return n <= 0 || m <= 0 ? 0 : n*m
+}
+
+// what I learned from others' solutions
+
+export function paperwork2(n: number, m :number): number{
+    return (n > 0 && m > 0) && (n * m) || 0 // if n and m are greater than zero, returns n*m - OR 0, otherwise
+  }
